bdce6f9f822938c90011d4c81d82966160ae4d14
Daniel <d.mesa.olmo@accenture.com>
2020-05-18 10:19:07 +0200

    fix test

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev')
     urlDB = 'mongodb://localhost:27017/cafe';
 else
-    urlDB = 'mongodb+srv://dmesa:NeiM..4tGwHFWd6@cluster0-gb28e.mongodb.net/test?retryWrites=true&w=majority';
+    urlDB = 'mongodb+srv://dmesa:NeiM..4tGwHFWd6@cluster0-gb28e.mongodb.net/cafe?retryWrites=true&w=majority';
 
 process.env.URLDB = urlDB
 
